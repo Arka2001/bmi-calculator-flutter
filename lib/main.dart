135dc9c7c70e216bd299e0ce988d1aa9f150caf3
Sayantan Dasgupta <absarka2001@gmail.com>
2021-04-01 20:44:30 +0530

    Completed BMI Calculator App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'file:///C:/Users/User/AndroidStudioProjects/bmi-calculator-flutter/lib/screens/input_page.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
